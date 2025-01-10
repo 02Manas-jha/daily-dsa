79d190c4d98be00d94efb52a7965c2deb05553c3
Manas <me@example.com>
2025-01-10 23:25:20 +0530

    3rd day

---
+++ strings2.py
@@ -0,0 +1,26 @@
+"""
+Find non repeating character
+Examples:
+
+Example 1:
+Input: string = “google”
+Output: l,e
+Explanation: Non repeating characters are l,e.
+
+Example 2:
+Input: string = “yahoo”
+Output: y,a,h
+Explanation: Non repeating characters are y,a,h
+"""
+
+def soln(string):
+    freq = {}
+    for char in string:
+        freq[char] = freq.get(char, 0) + 1
+    for i, char in enumerate(string):
+        if freq[char] == 1:
+            print(char, end="")
+    return
+
+inp = input()
+print(soln(inp))
