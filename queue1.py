3e05edf5342c479a48c010ad3af0dd3893cc0d6d
Manas <manasadutiya@gmail.com>
2025-01-18 23:42:05 +0530

    6th day

--- queue1.py
+++ queue1.py
@@ -44,9 +44,39 @@ class Queue1:
 """
 Approach 2
 
+enQueue(q,  x)
+  1) Push x to stack1 (assuming size of stacks is unlimited).
+Here time complexity will be O(1)
 
+deQueue(q)
+  1) If both stacks are empty then error.
+  2) If stack2 is empty
+       While stack1 is not empty, push everything from stack1 to stack2.
+  3) Pop the element from stack2 and return it.
+Here time complexity will be O(n)
 
 """
+#Time - O(N+1)
+#space - O(N)
+class Queue2:
+
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def enQueue(self,x):
+        self.s1.append(x)
+    
+    def deQueue(self):
+        if len(self.s1) == 0 and len(self.s2) == 0:
+            return -1
+        elif len(self.s2) == 0 and len(self.s1) > 0:
+            while len(self.s1):
+                temp = self.s1.pop()
+                self.s2.append(temp)
+            return self.s2.pop()
+        else:
+            return self.s2.pop()
 
 
 if __name__ == '__main__':
@@ -58,3 +88,12 @@ if __name__ == '__main__':
     print(q.deQueue())
     print(q.deQueue())
     print(q.deQueue())
+
+    q2 = Queue2()
+    q2.enQueue(1)
+    q2.enQueue(2)
+    q2.enQueue(3)
+
+    print(q2.deQueue())
+    print(q2.deQueue())
+    print(q2.deQueue())
